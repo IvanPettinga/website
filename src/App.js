c121f25e2f912ea6589e05f535933a9dc2037184
Ivan <ivanpettinga@outlook.com>
2023-12-21 14:46:41 +0100

    Added title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <title>IvanPettinga|Begeleiding & Consultancy</title>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>testing out this code</h1>
